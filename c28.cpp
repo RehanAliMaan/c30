33faa2a83020565223475708f3b60c5c446146e7
Rehan Ali maan <130174371+RehanAliMaan@users.noreply.github.com>
2023-11-12 22:08:48 +0500

    Add files via upload

---
+++ c28.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main ()
+{
+	int n;
+	cout<<"enter your number"<<endl;
+	switch(n)
+	{
+		case'a':
+			case'A':
+				cout<<"you entered vovel";
+				break;
+			    case'e':
+			case'E':
+				cout<<"you entered vovel";
+				break;
+					case'i':
+			case'I':
+				cout<<"you entered vovel";
+				break;
+					case'o':
+			case'O':
+				cout<<"you entered vovel";
+				break;
+				
+					case'u':
+			case'U':
+				cout<<"you entered vovel";
+				break;	
+				cout<<"invalid number";	
+	}
+}
